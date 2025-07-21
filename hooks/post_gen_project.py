eae1c7738eee8870b5adfd31bcfe2f070bb4ccb5
Jonathan McKenzie <jmckenzie@mapc.org>
2025-07-21 18:14:34 -0400

    Fix typo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -28,7 +28,7 @@ packages_to_install = copy(packages)
 
 # {% if cookiecutter.include_code_scaffold == "Yes" %}
 packages_to_install += scaffold
-packages_to_installl += ["sqlalchemy", "psycopg2", "jupyter"]
+packages_to_install += ["sqlalchemy", "psycopg2", "jupyter"]
 # {% endif %}
 
 # {% if cookiecutter.pydata_packages == "basic" %}
